660c243830d97b7551be2b16d50e55ab1cedf03e
PERABOY A.K.A IcyHot <139584595+IcyHot9@users.noreply.github.com>
2026-01-08 18:48:38 +0600

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -1,32 +1,83 @@
-import java.util.ArrayList;
+import java.io.*;
 
 public class StudentManager {
 
-    private ArrayList<Student> students = new ArrayList<>();
+    private Student[] students = new Student[50];
+    private int count = 0;
+    private final String FILE_NAME = "students.txt";
 
-    public void addStudent(Student student) {
-        students.add(student);
+    public StudentManager() {
+        loadFromFile();
+    }
+
+    public void addStudent(Student s) {
+        if (count < students.length) {
+            students[count] = s;
+            count++;
+            saveToFile();
+        }
     }
 
     public Student searchStudent(String id) {
-        for (Student s : students) {
-            if (s.getId().equals(id)) {
-                return s;
+        for (int i = 0; i < count; i++) {
+            if (students[i].getId().equals(id)) {
+                return students[i];
             }
         }
         return null;
     }
 
     public boolean deleteStudent(String id) {
-        Student s = searchStudent(id);
-        if (s != null) {
-            students.remove(s);
-            return true;
+        for (int i = 0; i < count; i++) {
+            if (students[i].getId().equals(id)) {
+                for (int j = i; j < count - 1; j++) {
+                    students[j] = students[j + 1];
+                }
+                students[count - 1] = null;
+                count--;
+                saveToFile();
+                return true;
+            }
         }
         return false;
     }
 
-    public ArrayList<Student> getAllStudents() {
+    public Student[] getStudents() {
         return students;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+
+    private void saveToFile() {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME));
+            for (int i = 0; i < count; i++) {
+                bw.write(students[i].getId() + "," +
+                         students[i].getName() + "," +
+                         students[i].getDepartment());
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Error writing file");
+        }
+    }
+
+    private void loadFromFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                students[count] = new Student(data[0], data[1], data[2]);
+                count++;
+            }
+            br.close();
+        } catch (IOException e) {
+        }
     }
 }
